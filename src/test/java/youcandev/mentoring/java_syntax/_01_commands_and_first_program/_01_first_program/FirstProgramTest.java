70623247e44e772b1de9c71c817d5d3f08eaa95b
Michał Chodorowski <svennn1996@gmail.com>
2023-07-10 23:29:47 +0200

    add more unit tests for FirstTask

--- src/test/java/youcandev/mentoring/java_syntax/_01_commands_and_first_program/_01_first_program/FirstProgramTest.java
+++ src/test/java/youcandev/mentoring/java_syntax/_01_commands_and_first_program/_01_first_program/FirstProgramTest.java
@@ -6,11 +6,12 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class FirstProgramTest {
 
     @Test
-    public void shouldReturnDesiredTextFromSystemOutPrintln() {
+    void shouldReturnDesiredTextFromSystemOutPrintln() {
         //given
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outputStream));
@@ -22,5 +23,51 @@ class FirstProgramTest {
 
         //then
         assertEquals(expectedText, printedText);
+    }
+
+    @Test
+    void shouldStartFromMy() {
+        //given
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
+        //when
+        FirstProgram.main(new String[]{});
+        String printedText = outputStream.toString().trim();
+        String expectedText = "My";
+
+        //then
+        assertTrue(printedText.startsWith(expectedText));
+
+    }
+
+    @Test
+    void shouldEndWithWorks() {
+        //given
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
+        //when
+        FirstProgram.main(new String[]{});
+        String printedText = outputStream.toString().trim();
+        String expectedText = "works!";
+
+        //then
+        assertTrue(printedText.endsWith(expectedText));
+    }
+
+    @Test
+    void shouldHas23Length() {
+        //given
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
+        //when
+        FirstProgram.main(new String[]{});
+        String printedText = outputStream.toString().trim();
+        int expectedLength = 23;
+
+        //then
+        assertEquals(printedText.length(), expectedLength);
     }
 }
